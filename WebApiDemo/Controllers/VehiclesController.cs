98f3f37f67627a0c4b42bcc1d015a906c4cd5114
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-02-16 09:38:36 +0200

    Add project files.

---
+++ WebApiDemo/Controllers/VehiclesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VehiclesController : ControllerBase
+    {
+        [HttpGet]
+        public string GetVehicles()
+        {
+            return $"Reading all the vehicles.";
+        }
+
+        [HttpGet("{id}")]
+        public string GetVehicleById(int id)
+        {
+            return $"Reading information for vehicle with ID: {id}";
+        }
+
+        [HttpPost]
+        public string CreateVehicle()
+        {
+            return $"Creating a new vehicle";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateVehicle(int  id)
+        {
+            return $"Updating vehicle with ID: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteVehicle(int id)
+        {
+            return $"Deleting vehicle with ID: {id}";
+        }
+    }
+}
