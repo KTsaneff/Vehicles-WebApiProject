6c7fead0b29037e67ac6af75842e5f7e20e74a6d
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-02-22 08:09:42 +0200

    NuGet_Packages_Added: The application will work with external db.

---
+++ WebApiDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Vehicle>()
+                .Property(v => v.Engine)
+                .HasConversion<string>();
+
+            //dataseeding
+        }
+    }
+}
